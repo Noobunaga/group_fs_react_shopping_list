7f726d0addede64b8724e85c93fe35d93a99ff38
Pouapheng Yang <phewy06@yahoo.com>
2021-07-09 10:13:18 -0500

    added databse

--- database.sql
+++ database.sql
@@ -1,2 +1,11 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE shopping_list (
+    "id" serial PRIMARY KEY,
+    "name" varchar(80),
+    "quantity" DECIMAL(p,s),
+    "unit" varchar(20),
+    "purchased" boolean default 'false'
+)
+
