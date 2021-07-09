1a0faf4a0c7ef2def959c7cbef0b73939a95f8d6
John Idso <john.d.idso@gmail.com>
2021-07-09 15:32:37 -0500

    conditional formatting functional

--- src/components/ShoppingItem/ShoppingItem.jsx
+++ src/components/ShoppingItem/ShoppingItem.jsx
@@ -2,12 +2,17 @@ import react from 'react';
 import '../ShoppingItem/ShoppingItem.css';
 
 function ShoppingItem(props){
-    return (
+    return ( (!props.item.purchased) ? (
         <li>
-            {props.item.name} {props.item.quantity} {props.item.unit}
+            {props.item.name} {props.item.quantity} {props.item.unit} {props.item.purchased}
             <button onClick={() => props.itemPurchased(props.item.id)} >Buy</button>
             <button class="removeButton">Remove</button>
         </li>
+        ) : (
+        <li>
+            {props.item.name} {props.item.quantity} {props.item.unit} {props.item.purchased}
+        </li>
+        )
     )
 }
 
