289d97defc3da293ebcd08599d5ac93b60218efa
Hann1234 <christopherjhannigan@gmail.com>
2021-07-09 11:34:46 -0500

    completed GET and POST routes

--- database.sql
+++ database.sql
@@ -1,2 +1,16 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+React Shopping List
+
+CREATE TABLE shopping_list (
+    "id" serial PRIMARY KEY,
+    "name" varchar(80),
+    "quantity" DECIMAL(p,s),
+    "unit" varchar(20),
+    "purchased" boolean default 'false'
+);
+
+INSERT INTO shopping_list ("name", "quantity", "unit")
+VALUES ('milk', '1', 'gallon'),
+('eggs', '1', 'dozen'),
+('beef', '1', 'pound');
--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,5 +4,40 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
+// GET route to get list items from database
+router.get('/', (req, res) => {
+    // When you fetch all things in these GET routes, strongly encourage ORDER BY
+    // so that things always come back in a consistent order
+    //create variable to hold SQL query 
+    const sqlText = `SELECT * FROM shopping_list ORDER BY id DESC;`;
+    pool.query(sqlText)
+        .then((result) => {
+            console.log(`Got stuff back from the database`, result);
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500); // Good server always responds
+        })
+})
+
+// POST-INSERT: adds a new item to the database list
+
+router.post('/', (req, res) => {
+    const item = req.body;
+    const sqlText = `INSERT INTO shopping_list ("name", "quantity", "unit")
+                     VALUES ($1, $2, $3)`;
+    
+    pool.query(sqlText, [item.name, item.quantity, item.unit])
+        .then((result) => {
+            console.log(`Added grocery item to the database`, item);
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500); // Good server always responds
+        })
+})
+
 
 module.exports = router;
