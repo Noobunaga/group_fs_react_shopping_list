0fd84b27b39f5201cd87a2637594cf0654f7be1a
Tim Dugan <tadugan@me.com>
2021-07-09 14:57:54 -0500

    Added reset button functionality. Resets all items to purchased: false.

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -5,7 +5,7 @@ function ShoppingList(props){
     return (
         <>
             <h2>Shopping List</h2>
-                <button id="resetButton">Reset</button>
+                <button onClick={props.resetPurchased} id="resetButton">Reset</button>
                 <button id="clearButton">Clear</button>
                 <ul>
                     {props.list.map(item => 
