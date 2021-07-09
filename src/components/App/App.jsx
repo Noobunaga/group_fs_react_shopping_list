5045abe1120372ac43b9814774cc00b2de63d777
John Idso <john.d.idso@gmail.com>
2021-07-09 11:28:12 -0500

    added GET

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
+import axios from 'axios';
 
-import Header from '../Header/Header.jsx'
+import ShoppingList from '../ShoppingList/ShoppingList.jsx';
+import Header from '../Header/Header.jsx';
 import './App.css';
 
 
 function App() {
+    const [shoppingList, setShoppingList] = useState([]);
+    const getList = () => {
+        axios({
+            method: 'GET',
+            url: '/list'
+        })
+        .then( response => {
+            console.log('GET data', response.data);
+            setShoppingList(response.data);
+        })
+        .catch( err => {
+            console.log('Error during GET', err);
+        });
+    }
+
+
     return (
         <div className="App">
             <Header />
             <main>
-                <p>Under Construction...</p>
+                <h1>Add an Item:</h1>
+                {/* Inputs */}
+                <ShoppingList list={shoppingList} />
             </main>
         </div>
     );
